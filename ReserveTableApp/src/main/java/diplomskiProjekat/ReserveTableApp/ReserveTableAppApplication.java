1989b9075f563a73e6a49f0835dedb321b9d714e
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-03 09:30:18 +0200

    initial commit

---
+++ ReserveTableApp/src/main/java/diplomskiProjekat/ReserveTableApp/ReserveTableAppApplication.java
@@ -0,0 +1,13 @@
+package diplomskiProjekat.ReserveTableApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveTableAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveTableAppApplication.class, args);
+	}
+
+}
---
+++ ReserveTableApp/src/test/java/diplomskiProjekat/ReserveTableApp/ReserveTableAppApplicationTests.java
@@ -0,0 +1,13 @@
+package diplomskiProjekat.ReserveTableApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveTableAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
