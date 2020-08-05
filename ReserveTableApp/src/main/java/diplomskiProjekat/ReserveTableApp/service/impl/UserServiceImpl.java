d442300ec987d3ebb7a6f3bd129b07cdec3a55f4
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-05 16:02:02 +0200

    created user service implementation

---
+++ ReserveTableApp/src/main/java/diplomskiProjekat/ReserveTableApp/service/impl/UserServiceImpl.java
@@ -0,0 +1,87 @@
+package diplomskiProjekat.ReserveTableApp.service.impl;
+
+import diplomskiProjekat.ReserveTableApp.dto.CustomerDTO;
+import diplomskiProjekat.ReserveTableApp.model.Authority;
+import diplomskiProjekat.ReserveTableApp.model.Customer;
+import diplomskiProjekat.ReserveTableApp.model.User;
+import diplomskiProjekat.ReserveTableApp.model.enums.Role;
+import diplomskiProjekat.ReserveTableApp.repository.CustomerRepository;
+import diplomskiProjekat.ReserveTableApp.repository.UserRepository;
+import diplomskiProjekat.ReserveTableApp.service.AuthorityService;
+import diplomskiProjekat.ReserveTableApp.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthorityService authorityService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User findByUsername(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public boolean register(CustomerDTO customer) {
+        if(userRepository.findByEmail(customer.getEmail()) == null){
+            Customer c = modelMapper.map(customer,Customer.class);
+            c.setPassword(passwordEncoder.encode(customer.getPassword()));
+            c.setRole(Role.CUSTOMER);
+            Authority authoritie = authorityService.findByname("CUSTOMER");
+            List<Authority> authorities = new ArrayList<>();
+            authorities.add(authoritie);
+            c.setAuthorities(authorities);
+
+            if(customerRepository.save(c) != null){
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = (userRepository.findByEmail(email));
+
+        if(user == null)
+            throw new UsernameNotFoundException("User with "+ email+" doesn't exists!");
+
+        return user;
+    }
+}
