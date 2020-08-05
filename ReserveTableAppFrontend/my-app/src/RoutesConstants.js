fae095a72b2b76b2685000808e7bdacd7860e9d7
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-05 16:06:49 +0200

    created routes constants

---
+++ ReserveTableAppFrontend/my-app/src/RoutesConstants.js
@@ -0,0 +1,7 @@
+export const home =  '/';
+export const registration = '/registration';
+export const login =  '/login';
+export const facilites =  '/facilites';
+export const facility =  '/facility';
+export const newfacility =  '/new-facility';
+export const facilityid = '/facility/:id';
