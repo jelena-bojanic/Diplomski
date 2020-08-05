5e2afb1b1616ec3387a681db94c8de1f62bc335e
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-05 15:55:06 +0200

    created adminDTO class

---
+++ ReserveTableApp/src/main/java/diplomskiProjekat/ReserveTableApp/dto/AdminDTO.java
@@ -0,0 +1,79 @@
+package diplomskiProjekat.ReserveTableApp.dto;
+
+import diplomskiProjekat.ReserveTableApp.model.enums.Role;
+
+import javax.persistence.*;
+
+public class AdminDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+    public AdminDTO(Long id, String name, String lastname, String email, String password, Role role) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public AdminDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
