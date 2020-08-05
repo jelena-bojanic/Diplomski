714b82f8dc51eac54b7cf0d149ad57369396f621
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-05 16:07:30 +0200

    updated app component

--- ReserveTableAppFrontend/my-app/src/App.js
+++ ReserveTableAppFrontend/my-app/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Routes from './Routes';
+import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.updateUser = this.updateUser.bind(this)
+    this.state = {
+      user: {},
+    }
+  }
+
+  componentDidMount(){
+  this.updateUser(localStorage.getItem('token'));
+  }
+
+  async updateUser(token){
+    if (token === null || token === undefined) {
+      this.setState({user:undefined});
+    }else{
+
+    const options = {
+      headers: { 'Authorization': 'Bearer ' + token}
+    };
+    const [firstResponse] = await Promise.all([
+      axios.get('http://localhost:8081/auth/user', options),
+    ]);
+
+      if(firstResponse.data !== null) {this.changeState(firstResponse); }
+      else{
+            if(window.location.href !== 'http://localhost:3000/login' &&
+             window.location.href !== 'http://localhost:3000/')
+             alert('logged out');
+      }     
+          
+    }
+  }
+
+  changeState = (resp) => {
+
+      this.setState({user: resp.data});
+  }
+
+  clearState = () => {
+    this.setState({user: undefined});
+  }
+
+    render(){
+
+      return (
+        <div>
+          <Router>
+            <Routes user={this.state.user} clearState={this.clearState} updateUser={this.updateUser}/>
+          </Router>
+        </div>
+      );
+    }
+  }
+
