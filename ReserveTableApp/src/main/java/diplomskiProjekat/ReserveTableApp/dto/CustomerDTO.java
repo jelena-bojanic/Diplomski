5c34f107d86fe685272be38e5cfacc8dc14d22f4
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-05 15:55:16 +0200

    created customerDTO class

---
+++ ReserveTableApp/src/main/java/diplomskiProjekat/ReserveTableApp/dto/CustomerDTO.java
@@ -0,0 +1,95 @@
+package diplomskiProjekat.ReserveTableApp.dto;
+
+import diplomskiProjekat.ReserveTableApp.model.Customer;
+import diplomskiProjekat.ReserveTableApp.model.Reservation;
+import diplomskiProjekat.ReserveTableApp.model.enums.Role;
+
+import java.util.List;
+
+public class CustomerDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+    private List<ReservationDTO> reservations;
+
+    public CustomerDTO(){}
+
+    public CustomerDTO(Customer customer){
+        this.id = customer.getId();
+        this.name = customer.getName();
+        this.lastname = customer.getLastname();
+        this.email = customer.getEmail();
+        this.password = customer.getPassword();
+        this.role = customer.getRole();
+
+        for(Reservation r : customer.getReservations()){
+            ReservationDTO dto = new ReservationDTO();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<ReservationDTO> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<ReservationDTO> reservations) {
+        this.reservations = reservations;
+    }
+}
