08c7406a45d8d6a8ea1c5ca7ddd23542a8baea78
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-08-24 13:53:57 +0200

    added key to map

--- ReserveTableAppFrontend/my-app/src/components/reservations/ViewMyReservations.js
+++ ReserveTableAppFrontend/my-app/src/components/reservations/ViewMyReservations.js
@@ -27,7 +27,8 @@ class ViewMyReservations extends React.Component {
         return this.props.user.reservations.map(r =>{
             if(moment(r.reservationDate[0]+"-"+r.reservationDate[1]+"-"+r.reservationDate[2], 'YYYY-MM-DD').hours(r.startReservation[0]).minutes(r.startReservation[1]).isAfter(moment())){
                 console.log(moment().subtract(r.duration,"minutes"));
-            return(<Card className="singleCard"  style={{marginLeft:'2%',padding:'15px',height:'auto',width:'auto',cursor:'pointer',marginTop:'1.5%'}}>
+            return(
+            <Card key={r.id} className="singleCard"  style={{marginLeft:'2%',padding:'15px',height:'auto',width:'auto',cursor:'pointer',marginTop:'1.5%'}}>
                 <Card.Img src={bell}style={{height:'55px',width:'55px',width:'100%'}}></Card.Img>
                 <Card.Body>
                     <p>Reservation date: {r.reservationDate[0]}.{r.reservationDate[1]}.{r.reservationDate[2]}</p> 
@@ -40,7 +41,8 @@ class ViewMyReservations extends React.Component {
                 </Card.Body>
             </Card>);
             }else{
-                return(<Card className="singleCard"  style={{marginLeft:'2%',padding:'15px',height:'auto',width:'auto',cursor:'pointer',marginTop:'1.5%'}}>
+                return(
+                <Card key={r.id} className="singleCard"  style={{marginLeft:'2%',padding:'15px',height:'auto',width:'auto',cursor:'pointer',marginTop:'1.5%'}}>
                 <Card.Img src={bell}style={{height:'55px',width:'55px',width:'100%'}}></Card.Img>
                 <Card.Body>
                     <p>Reservation date: {r.reservationDate[0]}.{r.reservationDate[1]}.{r.reservationDate[2]}</p> 
